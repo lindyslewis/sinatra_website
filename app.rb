64a53437781e7ae1455e986e2b1d596a2a747282
Lindy Lewis <lindyslewis@gmail.com>
2018-02-08 16:51:37 -0600

    clean up app file, create public/ & views/

--- app.rb
+++ app.rb
@@ -1,15 +1,6 @@
 #app.ru
 require 'sinatra'
-require 'net/http'
-
-set :bind, '0.0.0.0'
-
-url =  'http://169.254.169.254/latest/meta-data/instance-id' 
-
-uri = URI(url)
-#output = Net::HTTP.get(uri) # => String
-output = "Localhost"
 
 get '/' do 
-  "hello world from instance number: #{output} "
+  "Photography Blog"
 end
