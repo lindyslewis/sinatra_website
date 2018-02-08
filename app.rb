d4d9b9445dcc73fcffb322de35e0bd834dddc44f
Lindy Lewis <lindyslewis@gmail.com>
2018-02-08 16:56:18 -0600

    commented default file paths

--- app.rb
+++ app.rb
@@ -1,6 +1,11 @@
 #app.ru
 require 'sinatra'
 
+# Default file paths
+# set :root, File.dirname(__FILE__)
+# set :public_folder, settings.root + "public"
+# set :views, settings.root + "views"
+
 get '/' do 
   "Photography Blog"
 end
